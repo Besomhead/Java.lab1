86eccf0d643fe825e93b8d03874d90a41511fad3
Besomhead <gorshokov@mail.ru>
2016-02-27 01:19:37 +0300

    Revert "Wrong commit"
    This reverts commit 5f124b8d9772c9bcff9ffe54ad70f5dd476984fb.

--- src/Widget.java
+++ src/Widget.java
@@ -10,13 +10,8 @@ public class Widget
     static JTextField textedit = new JTextField(30);
     static JTextField textpanel = new JTextField(30);
     static JButton swap1 = new JButton("Enter");
-    static JButton swap2 = new JButton("Enter button name");
+    static JButton swap2 = new JButton("Second button");
     static JComboBox combobox = new JComboBox();
-    static JTextField textField = new JTextField(30);
-    static JButton select = new JButton("Select");
-    static JRadioButton radioOption1 = new JRadioButton("15-18");
-    static JRadioButton radioOption2 = new JRadioButton("19-22");
-    static JRadioButton radioOption3 = new JRadioButton("23-26");
 
     public static void main(String[] args)
     {
@@ -26,7 +21,7 @@ public class Widget
 
         FirstTabCreating();
         SecondTabCreating();
-        ThirdTabCreating();
+
 
         frame.add(tabs);
 
@@ -109,58 +104,6 @@ public class Widget
             String temp = swap2.getText();
             swap2.setText(swap1.getText());
             swap1.setText(temp);
-        }
-    }
-
-    public static void ThirdTabCreating()
-    {
-        JPanel tab3 = new JPanel(new BorderLayout());
-        JPanel linePanel = new JPanel(new FlowLayout());
-        linePanel.setBackground(new Color(9, 55, 9));
-        JPanel buttonBox = new JPanel(new GridLayout(3, 1));
-        JLabel question = new JLabel("How old are you?");
-
-        linePanel.add(textField);
-        linePanel.add(select);
-
-        select.addActionListener(new SelectActionListener());
-
-        buttonBox.add(radioOption1);
-        buttonBox.add(radioOption2);
-        buttonBox.add(radioOption3);
-
-        ButtonGroup buttonGroup = new ButtonGroup();
-        buttonGroup.add(radioOption1);
-        buttonGroup.add(radioOption2);
-        buttonGroup.add(radioOption3);
-
-        tab3.add(question, BorderLayout.NORTH);
-        tab3.add(buttonBox, BorderLayout.CENTER);
-        tab3.add(linePanel, BorderLayout.SOUTH);
-
-        tabs.add(tab3, "Group3");
-    }
-
-    static String currentOption = "";
-
-    public static class SelectActionListener implements ActionListener
-    {
-        public void actionPerformed(ActionEvent event)
-        {
-            if (!currentOption.equals(textField.getText()))
-            {
-                currentOption = textField.getText();
-                if (currentOption.equals(radioOption1.getText())) radioOption1.setSelected(true);
-                else
-                    if (currentOption.equals(radioOption2.getText())) radioOption2.setSelected(true);
-                    else
-                        if(currentOption.equals(radioOption3.getText())) radioOption3.setSelected(true);
-                        else
-                        {
-                            JOptionPane wrongOption = new JOptionPane();
-                            wrongOption.showMessageDialog(frame, "Sorry, this option does not exist. Please try again.");
-                        }
-            }
         }
     }
 }
