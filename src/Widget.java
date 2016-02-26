99f062303948f115fd3a3832a5874a87e1d3fe5a
Besomhead <gorshokov@mail.ru>
2016-02-26 16:32:21 +0300

    File structure was changed. Group2 completed

--- src/Widget.java
+++ src/Widget.java
@@ -1,24 +1,37 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
 public class Widget
 {
+    static JFrame frame = new JFrame("Widget");
+    static JButton enter = new JButton("Enter");
+    static JTabbedPane tabs = new JTabbedPane();
+    static JTextField textedit = new JTextField(30);
+    static JTextField textpanel = new JTextField(30);
+    static JButton swap1 = new JButton("Enter");
+    static JButton swap2 = new JButton("Second button");
+
     public static void main(String[] args)
     {
-        JFrame frame = new JFrame("Widget");
-
         frame.setSize(600, 400);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
 
-        JTabbedPane tabs = new JTabbedPane();
+        FirstTabCreating();
+        SecondTabCreating();
 
+
+        frame.add(tabs);
+
+        frame.setVisible(true);
+    }
+
+    public static void FirstTabCreating()
+    {
         JPanel tab1 = new JPanel(new BorderLayout());
         JPanel downpanel = new JPanel(new FlowLayout());
         downpanel.setBackground(new Color(9, 55, 9));
-
-        JButton enter = new JButton("Enter");
-        JTextField textedit = new JTextField(30);
 
         downpanel.add(textedit);
         downpanel.add(enter);
@@ -32,13 +45,14 @@ public class Widget
         tab1.add(combobox, BorderLayout.NORTH);
 
         tabs.add(tab1, "Group1");
+    }
 
+    public static void SecondTabCreating()
+    {
         JPanel tab2 = new JPanel(new BorderLayout());
 
-        JTextField textpanel = new JTextField(30);
-
-        JButton swap1 = new JButton("Enter");
-        JButton swap2 = new JButton("Second button");
+        swap1.addActionListener(new Swap1ActionListener());
+        swap2.addActionListener(new Swap2ActionListener());
 
         JPanel extrapanel = new JPanel(new FlowLayout());
         extrapanel.setBackground(new Color(9, 55, 9));
@@ -50,11 +64,24 @@ public class Widget
         tab2.add(swap2, BorderLayout.NORTH);
 
         tabs.add(tab2, "Group2");
-
-        frame.add(tabs);
-
-        frame.setVisible(true);
     }
 
+    public static class Swap1ActionListener implements ActionListener
+    {
+        public void actionPerformed(ActionEvent event)
+        {
+            swap2.setText(textpanel.getText());
+            textpanel.setText("");
+        }
+    }
 
+    public static class Swap2ActionListener implements ActionListener
+    {
+        public void actionPerformed(ActionEvent event)
+        {
+            String temp = swap2.getText();
+            swap2.setText(swap1.getText());
+            swap1.setText(temp);
+        }
+    }
 }
